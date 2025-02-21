527a97ac6ab6165d2d6e287c8ac4142757b909c8
rina2004 <shinnolove.2004@gmail.com>
2025-02-21 14:13:55 +0700

    duc

--- src/java/Filter/AdminFilter.java
+++ src/java/Filter/AdminFilter.java
@@ -215,6 +215,7 @@ public class AdminFilter implements Filter {
                 ps.close();
                 response.getOutputStream().close();
             } catch (Exception ex) {
+                ex.printStackTrace();
             }
         }
     }
