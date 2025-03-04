10a858bbca4b63be2cbc7525b19176047a1183a8
rina2004 <shinnolove.2004@gmail.com>
2025-03-04 13:08:02 +0700

    ...

--- src/java/context/DBContext.java
+++ src/java/context/DBContext.java
@@ -1,6 +1,4 @@
-    package context;
-
-
+package context;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,25 +6,15 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
  * @author FPT University - PRJ30X
  */
 public class DBContext {
-    public Connection connection;
-    public DBContext()
-    {
-        //@Students: You are allowed to edit user, pass, url variables to fit 
-        //your system configuration
-        //You can also add more methods for Database Interaction tasks. 
-        //But we recommend you to do it in another class
-        // For example : StudentDBContext extends DBContext , 
-        //where StudentDBContext is located in dal package, 
+
+    protected Connection connection;
+
+    public DBContext() {
         try {
             String user = "rina";
             String pass = "26122004";
